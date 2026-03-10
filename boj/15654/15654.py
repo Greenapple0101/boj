6b35df96a2e61b3d3f9dca4d7f3cb8d74a242ff7
Greenapple0101 <yorange50@gmail.com>
2026-03-10 11:05:34 +0900

    n과m (5) (순열+리스트)

---
+++ boj/15654/15654.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+path = []
+visited_col = [0] * (n + 2)
+
+
+def dfs(level, start):
+    if (level == m):
+        print(*path)
+        return
+
+    for i in range(n):
+        if (visited_col[i] == 0):
+            path.append(lst[i])
+            visited_col[i] = 1
+            dfs(level + 1, i)
+            path.pop()
+            visited_col[i] = 0
+
+
+dfs(0, 0)
