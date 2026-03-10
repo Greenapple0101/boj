bc34bc7806ebbc830b5159c46c9a0b6196d51596
Greenapple0101 <yorange50@gmail.com>
2026-03-10 14:49:12 +0900

    dp 바텀업 기초

---
+++ boj/9461/9461.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    if (N <= 3):
+        print(1)
+        continue
+    dp = [0] * (N + 1)
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 1
+
+    for i in range(3, N):
+        dp[i] = dp[i - 2] + dp[i - 3]
+
+    print(dp[N - 1])
