f2791561751e4cbb268bab0150a7609e0ce3b64f
Greenapple0101 <yorange50@gmail.com>
2026-03-10 16:44:51 +0900

    dp knapsack

---
+++ boj/2294/2294.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+dp=[1e9]*(k+1)
+dp[0]=0
+
+for c in coins:
+  for i in range(c,k+1):
+    if(dp[i-c]!=1e9):
+      dp[i]=min(dp[i],dp[i-c]+1)
+
+
+if(dp[k]==1e9):
+  print(-1)
+else:
+  print(dp[k])
