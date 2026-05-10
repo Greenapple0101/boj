f30ea6beab82f4cffef2ec806e42df4528792e98
Greenapple0101 <105805445+Greenapple0101@users.noreply.github.com>
2026-05-10 22:39:51 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        cnt={}
+
+        for ch in s:
+            cnt[ch]=cnt.get(ch,0)+1
+
+        for ch in t:
+            if ch not in cnt:
+                return False
+            
+            cnt[ch]-=1
+
+            if cnt[ch]<0:
+                return False
+        
+        return True
